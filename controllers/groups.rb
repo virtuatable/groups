51a98820e25083134e1fcb18b0d8328ed2d0e3fb
babausse <courtois.vincent@outlook.com>
2018-02-10 11:34:57 +0100

    Add the possibility to delete groups

--- controllers/groups.rb
+++ controllers/groups.rb
@@ -2,18 +2,21 @@ module Controllers
   # Controller for the rights, mapped on /rights
   # @author Vincent Courtois <courtois.vincent@outlook.com>
   class Groups < Arkaan::Utils::Controller
+
+    ['/:id', '/:id/rights', '/:id/routes'].each do |path|
+      before path do
+        @group = Arkaan::Permissions::Group.where(id: params['id']).first
+        halt(404, {message: 'group_not_found'}.to_json) if @group.nil?
+      end
+    end
+
     declare_route 'get', '/' do
       groups = Decorators::Group.decorate_collection(Arkaan::Permissions::Group.all)
       halt 200, {count: Arkaan::Permissions::Group.count, items: groups.map(&:to_h)}.to_json
     end
 
     declare_route 'get', '/:id' do
-      group = Arkaan::Permissions::Group.where(id: params['id']).first
-      if group.nil?
-        halt 404, {message: 'group_not_found'}.to_json
-      else
-        halt 200, Decorators::Group.new(group).to_json
-      end
+      halt 200, Decorators::Group.new(@group).to_json
     end
 
     declare_route 'post', '/' do
@@ -27,33 +30,28 @@ module Controllers
     end
 
     declare_route 'patch', '/:id/rights' do
-      group = Arkaan::Permissions::Group.where(id: params['id']).first
-      if group.nil?
-        halt 404, {message: 'group_not_found'}.to_json
-      else
-        check_items(Arkaan::Permissions::Right, 'right')
-        group.rights = []
-        params['rights'].each do |right_id|
-          group.rights << Arkaan::Permissions::Right.where(id: right_id).first
-        end
-        group.save
-        halt 200, {message: 'updated'}.to_json
+      check_items(Arkaan::Permissions::Right, 'right')
+      @group.rights = []
+      params['rights'].each do |right_id|
+        @group.rights << Arkaan::Permissions::Right.where(id: right_id).first
       end
+      @group.save
+      halt 200, {message: 'updated'}.to_json
     end
 
     declare_route 'patch', '/:id/routes' do
-      group = Arkaan::Permissions::Group.where(id: params['id']).first
-      if group.nil?
-        halt 404, {message: 'group_not_found'}.to_json
-      else
-        check_items(Arkaan::Monitoring::Route, 'route')
-        group.routes = []
-        params['routes'].each do |route_id|
-          group.routes << Arkaan::Monitoring::Route.where(id: route_id).first
-        end
-        group.save
-        halt 200, {message: 'updated'}.to_json
+      check_items(Arkaan::Monitoring::Route, 'route')
+      @group.routes = []
+      params['routes'].each do |route_id|
+        @group.routes << Arkaan::Monitoring::Route.where(id: route_id).first
       end
+      @group.save
+      halt 200, {message: 'updated'}.to_json
+    end
+
+    declare_route 'delete', '/:id' do
+      @group.delete
+      halt 200, {message: 'deleted'}.to_json
     end
 
     def check_items(klass, singular)
